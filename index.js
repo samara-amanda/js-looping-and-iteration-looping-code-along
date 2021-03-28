e988a160b8499639d2d998e36482ed24d25525cc
Sam <samara.amanda01@gmail.com>
2021-03-28 16:17:22 -0400

    commit tree 5034043ac55f47042103036a47011e87ca6f9594

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+const names = [ 'Lisa', 'Kaitlin', 'Jan' ];
+const event = ['birthday'];
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+}
+writeCards(names, event);
